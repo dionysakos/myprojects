7eae99a737aa865e5ef2694c161ccbfd89961b03
Dionysis <dionysiskatsetis@gmail.com>
2025-09-04 15:37:30 +0300

    Cpp test

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>  
-using namespace std;
+ //test
 
 int main() {
-    cout << "Hello, I bought a MacBook" << endl; 
+    std:: cout << "Hello, I bought a MacBook" << std:: endl; 
     return 0; 
 }
